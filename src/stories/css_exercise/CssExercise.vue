bea9ba67a20b6104551727b9d8f65b07cdec3f67
Danny Li <Danny.Li@clemenger.com.au>
2017-11-15 17:41:14 +1100

    css exercise update

--- src/stories/css_exercise/CssExercise.vue
+++ src/stories/css_exercise/CssExercise.vue
@@ -2,18 +2,116 @@
   <body class="body">
     <title>Natours | Exciting tours for adventurous people</title>
     <header class="header">
-        <div class="logo-box">
-            <img src="./img/logo-white.png" alt="Logo" class="logo">
+        <div class="header__logo-box">
+            <img src="./img/logo-white.png" alt="Logo" class="header__logo">
         </div>
 
-        <div class="text-box">
+        <div class="header__text-box">
             <h1 class="heading-primary">
-                <span class="heading-primary-main">Outdoors</span>
-                <span class="heading-primary-sub">is where life happens</span>
+                <span class="heading-primary--main">Outdoors</span>
+                <span class="heading-primary--sub">is where life happens</span>
             </h1>
+
+            <a href="#" class="button button--white button--animated">Discover our tour</a>
         </div>
-           
     </header>
+
+	<main>
+		<section class="section-about">
+			<div class="u-center-text u-margin-bottom-8">
+				<h2 class="heading-secondary">
+					Exciting tours for adventurous people
+				</h2>
+			</div>
+
+			<div class="row">
+				<div class="col-1-of-2">
+					<h3 class="heading-tertiary">You're going to fall in love with nature</h3>
+					<p class="paragraph">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptatum excepturi aut in eligendi mollitia commodi officiis. Aliquam, dolores sit enim nostrum, eius necessitatibus cum nobis vel sequi qui nam.
+					</p>
+
+					<h3 class="heading-tertiary">Live adventurous like you never have before</h3>
+					<p class="paragraph">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptatum excepturi aut in eligendi mollitia commodi officiis. Aliquam, dolores sit enim nostrum, eius necessitatibus cum nobis vel sequi qui nam.
+					</p>
+				</div>
+				<div class="col-1-of-2">
+					Image
+				</div>
+			</div>
+		</section>
+	</main>
+
+	<!-- <section class="grid-test">
+		<div class="row">
+			<div class="col-1-of-2">
+				Col 1 of 2
+			</div>
+			<div class="col-1-of-2">
+				Col 1 of 2
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-1-of-3">
+				Col 1 of 3
+			</div>
+			<div class="col-1-of-3">
+				Col 1 of 3
+			</div>
+			<div class="col-1-of-3">
+				Col 1 of 3
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-1-of-3">
+				Col 1 of 3
+			</div>
+			<div class="col-2-of-3">
+				Col 2 of 3
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-1-of-4">
+				Col 1 of 4
+			</div>
+			<div class="col-1-of-4">
+				Col 1 of 4
+			</div>
+			<div class="col-1-of-4">
+				Col 1 of 4
+			</div>
+			<div class="col-1-of-4">
+				Col 1 of 4
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-1-of-4">
+				Col 1 of 4
+			</div>
+			<div class="col-1-of-4">
+				Col 1 of 4
+			</div>
+			<div class="col-2-of-4">
+				Col 2 of 4
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-1-of-4">
+				Col 1 of 4
+			</div>
+			<div class="col-3-of-4">
+				Col 3 of 4
+			</div>
+		</div>
+
+
+	</section> -->
   </body>
 </template>
 
@@ -22,71 +120,19 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-@import url("https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900");
-@import url("https://fonts.googleapis.com/css?family=Barlow+Condensed");
+@import "scss/abstracts/variables";
+@import "scss/abstracts/functions";
+@import "scss/abstracts/mixins";
 
-* {
-  margin: 0px;
-  padding: 0px;
-  box-sizing: border-box;
-}
+@import "scss/base/base";
+@import "scss/base/animations";
+@import "scss/base/typography";
+@import "scss/base/utilities";
 
-body {
-  font-family: "Lato", sans-serif;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 1.7;
-  color: #777;
-  padding: 30px;
-}
+@import "scss/pages/home";
 
-.header {
-  height: 95vh;
-  background-image: linear-gradient(
-      to right bottom,
-      rgba(126, 213, 111, 0.8),
-      rgba(40, 180, 133, 0.8)
-    ),
-    url("img/hero.jpg");
-  background-size: cover;
-  background-position: top;
-  clip-path: polygon(0 0, 100% 0, 100% 75vh, 0 100%);
-  position: relative;
-}
+@import "scss/components/button";
 
-.logo-box {
-  position: absolute;
-  top: 40px;
-  left: 40px;
-}
-
-.logo {
-  height: 35px;
-}
-
-.text-box {
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.heading-primary {
-  color: #fff;
-  text-transform: uppercase;
-
-  &-main {
-    display: block;
-    font-size: 60px;
-    font-weight: 400;
-    letter-spacing: 35px;
-  }
-
-  &-sub {
-    display: block;
-    font-size: 20px;
-    font-weight: 700;
-    letter-spacing: 17.4px;
-  }
-}
+@import "scss/layout/header";
+@import "scss/layout/grid";
 </style>
