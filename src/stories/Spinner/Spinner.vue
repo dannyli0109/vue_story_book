64162868529a6bb2afa2989f8e7bfc305d0c6c0b
Danny Li <dannyli0109@gmail.com>
2017-11-29 23:43:41 +1100

    spinner update

--- src/stories/Spinner/Spinner.vue
+++ src/stories/Spinner/Spinner.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="spinner">
-      <div class="dot-container" v-for="i in 18" :key="i" :style="{transform: `translateX(-50%) rotate(${(i-1)*20}deg)`}">
-            <div class="spinner__dot" :class="{'current-dot': i==current}"></div>
+      <div class="dot-container" v-for="i in array" :key="i" :style="{transform: `translateX(-50%) rotate(${(i)*20}deg)`}">
+            <div class="spinner__dot" :class="{
+                'current-dot': i==current, 
+                'current-dot-light': i==(current-1)%18, 
+                'current-dot-lighter': i==(current-2)%18,
+                'current-dot-lightest': i==(current-3)%18
+                }"></div>
       </div>
   </div>
 </template>
@@ -10,22 +15,26 @@
 export default {
   data() {
     return {
-      current: 0
+      current: -1,
+      arrayLength: 18
     };
   },
   watch: {
     current(val) {
       let vm = this;
-      if (this.current == 0) {
-        this.current == 1;
-      }
+
       setTimeout(() => {
-        vm.current = (vm.current + 1) % 19;
-      }, 80);
+        vm.current = (vm.current + 1) % 18;
+      }, 50);
     }
   },
   mounted() {
-    this.current = 1;
+    this.current = 0;
+  },
+  computed: {
+    array() {
+      return [...Array(this.arrayLength).keys()];
+    }
   }
 };
 </script>
